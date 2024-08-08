04dd8994d66b1b5064d9b3ddd24efb390e0b88a2
atasoylumedya <atasoylumedya@gmail.com>
2024-08-08 12:47:42 +0300

    Update db.js

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb+srv://atasoylumedya:12120012@cluster0.90i3v.mongodb.net/";
+  process.env.MONGODB_URI || "mongodb+srv://atasoylumedya:12120012@cluster0.90i3v.mongodb.net/guncelgirisler";
 
 if (!MONGODB_URI) {
   throw new Error(
